19c721f191344a12642ed6783a118e47a7830436
leioscx <124648467+leioscx@users.noreply.github.com>
2024-05-18 13:58:11 -0500

    Add files via upload

---
+++ src/session08/entidades/SmartTV.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package session08.entidades;
+
+import session08.interfaces.navegable;
+import session08.interfaces.reproducible;
+
+/**
+ *
+ * @author Estudiante
+ */
+public class SmartTV 
+        implements reproducible, navegable
+{
+
+    @Override
+    public void reproducible() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void navegar() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
---
+++ src/session08/entidades/Smartphone.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package session08.entidades;
+
+import session08.interfaces.camara;
+import session08.interfaces.navegable;
+import session08.interfaces.reproducible;
+
+/**
+ *
+ * @author Estudiante
+ */
+public class Smartphone
+        implements camara, navegable, reproducible
+{
+
+    @Override
+    public void tomarFoto() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void navegar() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void reproducible() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
